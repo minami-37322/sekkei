6d2374c9432dbee7ffd99739357e7a505fa8b49d
minami <minami.yajima@rakus-partners.co.jp>
2025-02-04 15:36:20 +0900

    second

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -58,7 +58,6 @@ public class EmployeeController {
 	public String showList(Model model) {
 		List<Employee> employeeList = employeeService.showList();
 		model.addAttribute("employeeList", employeeList);
-		model.addAttribute("employeeList", employeeList);
 		return "employee/list";
 	}
 
